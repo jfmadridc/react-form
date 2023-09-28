b05a73a28017066d965454bfe2acab72f7177be3
JuanF Madrid <jfm@jfmadrid.com>
2023-09-27 21:07:01 -0500

    Implementing the first material UI Component

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,8 +1,24 @@
+import { Button } from "@mui/material";
+
 function FormSignUp() {
 	return (
 		<form>
+			<label>First Name</label>
 			<input type="text" />
-			<button>Sign Up</button>
+
+			<label>Last name</label>
+			<input type="text" />
+
+			<label>Email</label>
+			<input type="email" />
+
+			<label>Promotions</label>
+			<input type="checkbox" />
+
+			<label>News</label>
+			<input type="checkbox" />
+
+			<Button variant="contained">Sign Up</Button>
 		</form>
 	);
 }
