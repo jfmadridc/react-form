8f02c94bf641fb1ce798918c0d966debb0ebd520
JuanF Madrid <jfm@jfmadrid.com>
2023-09-27 21:58:43 -0500

    implementing Component width size sm and body color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import FormSignUp from "./components/FormSignUp";
+import Container from "@mui/material/Container";
 
 function App() {
 	return (
-		<>
+		<Container component="section" maxWidth="sm">
 			<h1>Sign Up Form</h1>
 			<FormSignUp />
-		</>
+		</Container>
 	);
 }
 
